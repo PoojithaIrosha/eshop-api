8be6c894f8e0900f09d0d529c2881f12ecb684a7
poojithairosha <poojithairosha9311@gmail.com>
2023-11-09 18:48:29 +0530

    Secured all api endpoints

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', [\App\Http\Controllers\ProductController::class, 'index']);
-Route::get('products/{product}', [\App\Http\Controllers\ProductController::class, 'show']);
-
 Route::group(['namespace' => "App\Http\Controllers", 'middleware' => "auth:sanctum"], function () {
-    Route::post('products', [\App\Http\Controllers\ProductController::class, 'store']);
-    Route::put('products/{product}', [\App\Http\Controllers\ProductController::class, 'update']);
-    Route::patch('products/{product}', [\App\Http\Controllers\ProductController::class, 'update']);
-    Route::delete('products/{product}', [\App\Http\Controllers\ProductController::class, 'destroy']);
+    Route::apiResource("products", \App\Http\Controllers\ProductController::class);
 });
